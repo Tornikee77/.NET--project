7a15f92d37c4765e8d6357aea4140a746934190f
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-04-08 18:50:43 +0400

    feature/games dto and lissts

---
+++ .NET-PROJECT/Dtos/GameDto.cs
@@ -0,0 +1,8 @@
+namespace _NET_PROJECT.Dtos;
+
+public record class GameDto(
+    int Id, 
+    string Name, 
+    string Genre, 
+    decimal Price, 
+    DateOnly ReleaseDate);
--- .NET-PROJECT/Program.cs
+++ .NET-PROJECT/Program.cs
@@ -1,5 +1,28 @@
+using _NET_PROJECT.Dtos;
+
 var buiilder = WebApplication.CreateBuilder(args);  
 var app = buiilder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+List<GameDto> games = [
+    new (
+        1,
+        "mlbb",
+        "FARMING",
+        19.91M,
+        new DateOnly(1992,7,15)),
+          new (
+        2,
+        "Mortal Kombat",
+        "Fighting",
+        19.92M,
+        new DateOnly(1992,7,15)),
+          new (
+        3,
+        "FiFA",
+        "Football",
+        19.93M,
+        new DateOnly(1992,7,15)),
+];
+app.MapGet("/Games", () => games);
 app.Run();
